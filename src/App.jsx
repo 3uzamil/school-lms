2e01b7fc62f3a1a40ca9da1cc4c20d619689b15d
Muzamil Hussain <3uzamilhussain@gmail.com>
2026-03-21 11:27:20 +0500

    fix: ui in sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 export default function App() {
   return (
-    <div>
+    <>
       <Navbar />
       <Sidebar />
-    </div>
+    </>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,13 @@
 import {faCalendar,faFileLines,faHome,} from "@fortawesome/free-regular-svg-icons";
-import {faAngleLeft,faBookOpen,faBullhorn,faGraduationCap,} from "@fortawesome/free-solid-svg-icons";
+import {faAngleLeft,faBookOpen,faBullhorn,faGraduationCap, faWallet,} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 export default function Sidebar() {
   return (
     <section className="h-screen flex justify-start">
-      <div className="h-full w-50 bg-[#e5e5e5] border-r-20 border-[dodgerblue] flex pt-15">
-        <div className="translate-x-45 translate-y-10">
-          <FontAwesomeIcon icon={faAngleLeft} style={{color: "#e5e5e5"}} className="cursor-pointer"/>
-        </div>
-        <div className="flex flex-col gap-8 mt-15">
+      <div className="h-full w-50 bg-[#e5e5e5] flex gap-5 pl-5">
+        <div className="flex basis-[80%] flex-col gap-8 mt-20">
           <div className="flex items-center gap-2 cursor-pointer">
             <FontAwesomeIcon icon={faHome} />
             <h3>Dashboard</h3>
@@ -25,16 +22,23 @@ export default function Sidebar() {
           </div>
           <div className="flex items-center gap-2 cursor-pointer">
             <FontAwesomeIcon icon={faFileLines} />
-            <h3>Assignments</h3>
+            <h3>Assignment</h3>
           </div>
           <div className="flex items-center gap-2 cursor-pointer">
             <FontAwesomeIcon icon={faGraduationCap} />
-            <h3>Grades</h3>
+            <h3>Grade</h3>
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <FontAwesomeIcon icon={faWallet} />
+            <h3>Payment</h3>
           </div>
           <div className="flex items-center gap-2 cursor-pointer">
             <FontAwesomeIcon icon={faBullhorn} />
-            <h3>Announcements</h3>
+            <h3>Announcement</h3>
           </div>
+        </div>
+        <div className="bg-[dodgerblue] flex items-center">
+          <FontAwesomeIcon icon={faAngleLeft} className="cursor-pointer bg-white text-[dodgerblue] py-2"/>
         </div>
       </div>
     </section>
